0bd7718c570d392d6b99e23b5e79f1e48a803349
tavisca-dsheth <dsheth@tavisca.in>
2019-11-27 16:33:13 +0530

    Completed static UI page for employee

--- IMS-UI/ClientApp/src/app/app.module.ts
+++ IMS-UI/ClientApp/src/app/app.module.ts
@@ -1,17 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { RouterModule } from '@angular/router';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule, NgModel } from '@angular/forms';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    BrowserAnimationsModule
+  ]
+})
+export class MaterialModule { };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
+    MaterialModule,
     HttpClientModule,
     FormsModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+      { path: 'employee', component: EmployeeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

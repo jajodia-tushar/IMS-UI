2b73620abc5764d5b3ee7085298dd4ad0e0c9743
tavisca-dsheth <dsheth@tavisca.in>
2019-12-03 13:41:35 +0530

    Refactor the controller class

--- IMS-UI/Controllers/EmployeeValidationController.cs
+++ IMS-UI/Controllers/EmployeeValidationController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using IMS_UI.IMS.Providers;
 using IMS_UI.IMS.Models;
@@ -25,37 +21,3 @@ namespace IMS_UI.Controllers
         }
     }
 }
-
-//        // GET: api/EmployeeValidation
-//        [HttpGet]
-//        public IEnumerable<string> Get()
-//        {
-//            return new string[] { "value1", "value2" };
-//        }
-
-//        // GET: api/EmployeeValidation/5
-//        [HttpGet("{id}", Name = "Get")]
-//        public string Get(int id)
-//        {
-//            return "value";
-//        }
-
-//        // POST: api/EmployeeValidation
-//        [HttpPost]
-//        public void Post([FromBody] string value)
-//        {
-//        }
-
-//        // PUT: api/EmployeeValidation/5
-//        [HttpPut("{id}")]
-//        public void Put(int id, [FromBody] string value)
-//        {
-//        }
-
-//        // DELETE: api/ApiWithActions/5
-//        [HttpDelete("{id}")]
-//        public void Delete(int id)
-//        {
-//        }
-//    }
-//}

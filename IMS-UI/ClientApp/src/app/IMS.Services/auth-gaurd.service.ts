28bf2bc4d11fb60e8b4de49c0af8853e9606d196
tavisca-dkumar <dkumar@tavisca.in>
2019-12-03 15:40:28 +0530

    removed console.log mentioned in changes

--- IMS-UI/ClientApp/src/app/IMS.Services/auth-gaurd.service.ts
+++ IMS-UI/ClientApp/src/app/IMS.Services/auth-gaurd.service.ts
@@ -9,7 +9,7 @@ export class AuthGaurdService implements CanActivate {
 
   constructor(private service: LoginService, private route: Router) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    console.log(state.url)
+    
     if (this.service.isUserLoggedIn())
       return true;
     this.route.navigate(['login']);

cc3d75c6a880ca1704ca927f32e549a2a06cc5ff
Jajodia <tjajodia@tavisca.com>
2019-12-25 17:20:51 +0530

    Added Dynamic Data in the Filter Component

--- IMS-UI/ClientApp/src/app/IMS.Modules/admin/Components/reports-filter/reports-filter.component.ts
+++ IMS-UI/ClientApp/src/app/IMS.Modules/admin/Components/reports-filter/reports-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-reports-filter',
@@ -7,14 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsFilterComponent implements OnInit {
 
+
+  @Input()
+  dropdownListDatas : reportsDropDownListData[];
+
   constructor() { }
 
   ngOnInit() {
-  }
 
+    // this.dropdownListDatas = [
+    //     {
+    //       placeHolderName : "Shelf",
+    //       dropDownOptions : [
+    //         "Warehouse","First Floor","Sixth Floor"
+    //       ]
+    //     },
+    //     {
+    //       placeHolderName : "color",
+    //       dropDownOptions : [
+    //         "Red", "Amber", "Green"
+    //       ]
+    //     }
+    // ]
+  }
 }
 
 export interface Food {
   value: string;
   viewValue: string;
+}
+
+export class reportsDropDownListData{
+  placeHolderName : string;
+  dropDownOptions : string[];
+}
+
+export class reportsData{
+  reportName : string;
+  data : reportsDropDownListData[];
 }
--- IMS-UI/ClientApp/src/app/IMS.Modules/admin/Components/reports-tabs/reports-tabs.component.ts
+++ IMS-UI/ClientApp/src/app/IMS.Modules/admin/Components/reports-tabs/reports-tabs.component.ts
@@ -1,17 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { reportsData } from "../reports-filter/reports-filter.component";
 
 @Component({
-  selector: 'app-reports-tabs',
-  templateUrl: './reports-tabs.component.html',
-  styleUrls: ['./reports-tabs.component.css']
+  selector: "app-reports-tabs",
+  templateUrl: "./reports-tabs.component.html",
+  styleUrls: ["./reports-tabs.component.css"]
 })
 export class ReportsTabsComponent implements OnInit {
+  tabsNames: string[] = ["RAG", "Vendor", "Employee", "Item", "Shelf"];
 
-  tabsNames : string[] = ["RAG","Vendor","Employee","Item","Shelf"];
+  reportsSelectionData: reportsData[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.reportsSelectionData = [
+      {
+        reportName: "RAG",
+        data: [
+          {
+            placeHolderName: "Shelf",
+            dropDownOptions: ["Warehouse", "First Floor", "Sixth Floor"]
+          },
+          {
+            placeHolderName: "color",
+            dropDownOptions: ["Red", "Amber", "Green"]
+          }
+        ]
+      },
+      {
+        reportName: "Vendor",
+        data: [
+          {
+            placeHolderName: "VendorName",
+            dropDownOptions: ["Vendor 1", "Vendor 2", "Vendor 3"]
+          },
+          {
+            placeHolderName: "Date",
+            dropDownOptions: ["Select 1", "Select 2 ", "Select 3"]
+          }
+        ]
+      },
+      {
+        reportName: "Employee",
+        data: [
+          {
+            placeHolderName: "Employee Id",
+            dropDownOptions: ["1", "2", "3"]
+          },
+          {
+            placeHolderName: "Date",
+            dropDownOptions: ["Select 1", "Select 2 ", "Select 3"]
+          }
+        ]
+      },
+      {
+        reportName: "Item",
+        data: [
+          {
+            placeHolderName: "Item Name",
+            dropDownOptions: ["Pen", "Pencil", "Notebook"]
+          },
+          {
+            placeHolderName: "Date",
+            dropDownOptions: ["Select 1", "Select 2 ", "Select 3"]
+          }
+        ]
+      },
+      {
+        reportName: "Shelf",
+        data: [
+          {
+            placeHolderName: "ShelfName",
+            dropDownOptions: ["First Floor", "Sixth Floor"]
+          },
+          {
+            placeHolderName: "Date",
+            dropDownOptions: ["Select 1", "Select 2 ", "Select 3"]
+          }
+        ]
+      }
+    ];
   }
-
 }

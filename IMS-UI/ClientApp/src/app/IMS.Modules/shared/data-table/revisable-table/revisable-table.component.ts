5c99830b8e505723e41497f4b503eb121ddf6b2a
tavisca-sniyazi <51950508+tavisca-sniyazi@users.noreply.github.com>
2019-12-27 18:24:35 +0530

    added dropdown of items and calculate total amount function

--- IMS-UI/ClientApp/src/app/IMS.Modules/shared/data-table/revisable-table/revisable-table.component.ts
+++ IMS-UI/ClientApp/src/app/IMS.Modules/shared/data-table/revisable-table/revisable-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
+import { DataSource } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-revisable-table',
@@ -10,14 +11,22 @@ export class RevisableTableComponent implements OnInit {
 
   displayedColumns;
   datasource = new MatTableDataSource();
+ 
   @Input() columnHeader;
   @Input() show;
   @Input() set griddata(data){
     this.datasource= new MatTableDataSource(data);
-    
+   
   }
+ 
   @Output() deleteRow: EventEmitter<any> = new EventEmitter();
 
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
   // deletedata(data:any){
   //   let index = this.datasource.indexOf(data);
   //   if (index != -1) {
@@ -52,20 +61,70 @@ export class RevisableTableComponent implements OnInit {
   renderTable() {
     
     this.datasource = new MatTableDataSource(this.datasource.data);
+    
     console.log(this.datasource.data);
-    console.log(this.datasource);
+    
   }
 
   changeprice(row,event){
-     let index = this.datasource.data.indexOf(row);
-     row.totalPrice=(<HTMLInputElement>event.target).value;
+     console.log(row);
+     row.totalPrice= +(<HTMLInputElement>event.target).value;
+     this.renderTable();
+     
    }
 
    changequantity(row,event){
-    let index = this.datasource.data.indexOf(row);
+    
     row.quantity=(<HTMLInputElement>event.target).value;
+    this.renderTable();
    }
-  
+
+  //  AddRow() {
+  //   let itemData: ItemQuantityPriceMapping = {
+  //     item: { id: null, name: "", maxLimit: 0, isActive: false, imageUrl: "", rate: 0},
+  //     quantity: 0,
+  //     totalPrice:0
+  //   };
+  //   this.dataSourceItems.push(itemData);
+  //   this.renderTable();
+  //   console.log(this.dataSourceItems);
+  // }
+   AddRow(){
+     let datasoucelength=this.datasource.data.length;
+     console.log(datasoucelength);
+    this.datasource.data[datasoucelength]={
+        item: { id: null, name: "", maxLimit: 0, isActive: false, imageUrl: "", rate: 0},
+           quantity: 0,
+           totalPrice:0
+         };
+    this.renderTable();
+   }
+
+   selectedOption(row,event){
+    //  console.log(row);
+    //  console.log(row.item);
+    //  console.log(row.item.name);
+    //  console.log(event.value);
+     row.item.name=event.value;
+     this.renderTable();
+     
+     //console.log(event.value);
+    // row.item.name=event.item.value;
+    // row.item.name=
+   }
+
+//    selectedOption(event) {
+//     const selectedValue = event.option.value;
+//     console.log(selectedValue);
+//  }
+
+getTotalCost(){
+  let totalcost;
+ 
+   totalcost=this.datasource.data.map(t => t.totalPrice).reduce((acc, value) => acc + value, 0);
+  //console.log(totalcost);
+  return totalcost;
+}
   ngOnInit() {
     this.displayedColumns= this.columnHeader.map(c => c.columnDef)
     console.log(this.datasource.data);
@@ -77,4 +136,8 @@ export class RevisableTableComponent implements OnInit {
   } 
 
 
+}
+export interface Food {
+  value: string;
+  viewValue: string;
 }

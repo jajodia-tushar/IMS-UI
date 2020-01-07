055597a10cfacebc1432923b10953875526d9de4
tavisca-asingla <asingla@tavisca.in>
2020-01-07 13:05:55 +0530

    Increased snackbar messages

--- IMS-UI/ClientApp/src/app/IMS.Modules/shared/utils/snackbar.ts
+++ IMS-UI/ClientApp/src/app/IMS.Modules/shared/utils/snackbar.ts
@@ -1,16 +1,20 @@
 import { SnackbarComponent } from "../snackbar/snackbar.component";
 import { MatSnackBar } from "@angular/material";
 
-export function showMessage(snackBar: MatSnackBar, time: number, message:string, type="message"){
-    let extraClasses;
-      if (type == 'error') {
-     extraClasses = ['snackbar-background-red'];
-   } else {
-     extraClasses = ['snackbar-background-green'];
-   }
-    snackBar.openFromComponent(SnackbarComponent, {
-      duration: 1000 * time ,
-      panelClass : extraClasses,
-      data : { message : message }
-    });
+export function showMessage(snackBar: MatSnackBar, time: number, message: string, type = "message") {
+  let extraClasses = ['snackbar-message'];
+  if (type == 'warn') {
+    extraClasses = ['snackbar-warn'];
+  } 
+  else if(type=='success'){
+    extraClasses = ['snackbar-success'];
   }
+  else if(type=='message'){
+    extraClasses = ['snackbar-message'];
+  }
+  snackBar.openFromComponent(SnackbarComponent, {
+    duration: 1000 * time,
+    panelClass: extraClasses,
+    data: { message: message }
+  });
+}

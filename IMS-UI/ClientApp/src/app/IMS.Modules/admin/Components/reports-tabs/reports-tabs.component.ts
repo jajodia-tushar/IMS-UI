13f3335b1ede3215a4fd7191e958164b65375524
Tushar Jajodia <tjajodia@tavisca.com>
2020-01-27 10:31:55 +0530

    Spacing Refactoring

--- IMS-UI/ClientApp/src/app/IMS.Modules/admin/Components/reports-tabs/reports-tabs.component.ts
+++ IMS-UI/ClientApp/src/app/IMS.Modules/admin/Components/reports-tabs/reports-tabs.component.ts
@@ -333,8 +333,7 @@ export class ReportsTabsComponent implements OnInit {
               element.dataFromUser = this.toDate;
             }
           });
-      }
-    )
+      });
 
     await this.getRAGReportDropDownList().then(
       data => {

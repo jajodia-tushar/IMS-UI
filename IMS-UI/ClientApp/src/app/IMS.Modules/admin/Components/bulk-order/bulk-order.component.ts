4815cfed329040b72bd148111319848509c72e00
Tushar Jajodia <tjajodia@tavisca.com>
2020-01-31 13:31:23 +0530

    Added Table and Top Component in the Bulk Order Component

--- IMS-UI/ClientApp/src/app/IMS.Modules/admin/Components/bulk-order/bulk-order.component.ts
+++ IMS-UI/ClientApp/src/app/IMS.Modules/admin/Components/bulk-order/bulk-order.component.ts
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BulkOrderComponent implements OnInit {
 
-  constructor() { }
+  listOfFieldToBeDisplayed : FieldToBeDisplayed[] = [];
+  dataSource = [];
+  displayedColumns = ["itemName","itemQuantity","action"];
+  
+  numberOfColumns() {
+    return (this.displayedColumns.length - 1);
+  }
+  
+  constructor() { 
+
+    let x : FieldToBeDisplayed = {
+      fieldName : "Requested By",
+      fieldValue : "Shirin"
+    }
+
+    let y : FieldToBeDisplayed = {
+      fieldName : "Requested on",
+      fieldValue : "Today"
+    }
+
+    let z : TableData = {
+        itemName : "Pen",
+        itemQuantity : 500
+    }
+
+
+    this.listOfFieldToBeDisplayed.push(x);
+    this.listOfFieldToBeDisplayed.push(y);
+    this.listOfFieldToBeDisplayed.push(y);
+    this.listOfFieldToBeDisplayed.push(y);
+    this.listOfFieldToBeDisplayed.push(y);
+
+    this.dataSource.push(z);
+    this.dataSource.push(z);
+    this.dataSource.push(z);
+    this.dataSource.push(z);
+    this.dataSource.push(z);
+
+    
+  }
 
   ngOnInit() {
   }
 
+
+
+}
+
+
+export class FieldToBeDisplayed{
+  fieldName : string;
+  fieldValue : string;
+}
+
+export class TableData{
+  itemName : string;
+  itemQuantity : number;
 }

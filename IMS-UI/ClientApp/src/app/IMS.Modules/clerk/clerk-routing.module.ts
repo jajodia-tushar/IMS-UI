a00c46ac43843b28520fc849883dac2eb77e25a4
dsheth <dsheth@RENTLAPT130>
2020-02-02 17:23:56 +0530

    Added routing to chamge password for clerk

--- IMS-UI/ClientApp/src/app/IMS.Modules/clerk/clerk-routing.module.ts
+++ IMS-UI/ClientApp/src/app/IMS.Modules/clerk/clerk-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ClerkRouteGuardService } from 'src/app/IMS.Services/Route/clerk-route-guard.service';
 import { ClerkComponent } from './Components/clerk/clerk.component';
+import { ChangePasswordComponent } from '../shared/change-password/change-password.component';
 
 const routes: Routes = [{ path: '', component: ClerkComponent, canActivate:[ClerkRouteGuardService], data : { role : "Clerk"} }];
 

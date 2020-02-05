505bde7f1f3d47b1aadc8179e4a90087a5e653ec
tavisca-dkumar <dokumar@tavisca.com>
2020-02-05 16:23:26 +0530

    bug fixed on storing images of same name

--- IMS-UI/IMS.Providers/FileSystemStorage.cs
+++ IMS-UI/IMS.Providers/FileSystemStorage.cs
@@ -15,10 +15,24 @@ namespace IMS_UI.IMS.Providers
         {
             try
             {
-                var folderName = Path.Combine("Resources", "Images");
+                int count = 1;
+                var folderName = Path.Combine("Resources", "ChallanImages");
                 var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
                 var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
                 var fullPath = Path.Combine(pathToSave, fileName);
+                string fileNameOnly = Path.GetFileNameWithoutExtension(fullPath);
+                string path = Path.GetDirectoryName(fullPath);
+                string extension = Path.GetExtension(fullPath);
+                if (!Directory.Exists(pathToSave))
+                {
+                    Directory.CreateDirectory(pathToSave);
+                }
+                while (File.Exists(fullPath))
+                {
+                    string tempFileName = string.Format("{0}({1})", fileNameOnly, count++);
+                    fullPath = Path.Combine(path, tempFileName + extension);
+                    fileName = Path.GetFileName(fullPath);
+                }
                 var dbPath = Path.Combine(folderName, fileName);
 
                 using (FileStream stream = new FileStream(fullPath, FileMode.Create))
